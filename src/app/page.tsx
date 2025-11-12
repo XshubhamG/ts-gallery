d59b04d95519baa3e0413f000b43118f2b801819
Shubham Giri <shubhammgiri3@gmail.com>
2025-11-12 15:36:45 +0530

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { db } from "~/server/db";
 
@@ -11,23 +12,33 @@ const images = await db.query.images.findMany({
 export default async function HomePage() {
   return (
     <main>
-      <div className="grid grid-cols-3 gap-4 p-4">
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className="relative flex flex-col items-center justify-center gap-4 w-full h-full"
-          >
-            <Image
-              src={image.url}
-              alt={image.name}
-              width={100}
-              height={100}
-              className="object-cover w-full h-full"
-            />
-            <p className="text-white text-md font-bold">{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="flex flex-col items-center justify-center gap-4 w-full h-full p-4">
+          <h2 className="text-2xl font-bold">Welcome to TS Gallery</h2>
+          <p className="text-white text-md font-bold">
+            Please sign in to view images
+          </p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="grid grid-cols-3 gap-4 p-4">
+          {images.map((image) => (
+            <div
+              key={image.id}
+              className="relative flex flex-col items-center justify-center gap-4 w-full h-full"
+            >
+              <Image
+                src={image.url}
+                alt={image.name}
+                width={100}
+                height={100}
+                className="object-cover w-full h-full"
+              />
+              <p className="text-white text-md font-bold">{image.name}</p>
+            </div>
+          ))}
+        </div>
+      </SignedIn>
     </main>
   );
 }
